8aa02eaa4869069f45d03581a13ac042a674ebe4
sondt <sondt1610@gmail.com>
2021-08-28 22:39:50 +0700

    safdsa

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-// import {Provider} from 'react-redux'
-// import {store} from './state'
-// import RepositoriesList from './components/RepositoriesList';
+import {Provider} from 'react-redux'
+import {store} from './state'
+import RepositoriesList from './components/RepositoriesList';
 
 function App() {
   return (
-    // <Provider store={store}>
+    <Provider store={store}>
       <div className="App">
         <h1>Search for a package</h1>
-        {/* <RepositoriesList/> */}
+        <RepositoriesList/>
       </div>
-    // </Provider>
+    </Provider>
   );
 }
 
